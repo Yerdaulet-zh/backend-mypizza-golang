a1a46d69a2425fe63b01dec1f1d2d594d4b01d15
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-06-21 22:48:15 +0500

    KAN-5: define redis port

---
+++ internal/core/ports/redis.go
@@ -0,0 +1,20 @@
+package ports
+
+import (
+	"context"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type Redis interface {
+	// Satisfy the redis.Scripter interface
+	Eval(ctx context.Context, script string, keys []string, args ...interface{}) *redis.Cmd
+	EvalSha(ctx context.Context, sha1 string, keys []string, args ...interface{}) *redis.Cmd
+	EvalRO(ctx context.Context, script string, keys []string, args ...interface{}) *redis.Cmd
+	EvalShaRO(ctx context.Context, sha1 string, keys []string, args ...interface{}) *redis.Cmd
+	ScriptExists(ctx context.Context, hashes ...string) *redis.BoolSliceCmd
+	ScriptLoad(ctx context.Context, script string) *redis.StringCmd
+
+	Pipeline() redis.Pipeliner
+	Close() error
+}
