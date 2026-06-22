ef436eb39600c22ccb6ba65187dcc0280c5dca27
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-06-22 11:43:29 +0500

    KAN-5: add context to logger

--- internal/adapters/logging/stdout.go
+++ internal/adapters/logging/stdout.go
@@ -1,35 +1,57 @@
+// Package logging provides implementations for system logging adapters.
 package logging
 
 import (
+	"context"
 	"log/slog"
 	"os"
 
 	"github.com/yerdauletzhumabay/backend-mypizza-golang/internal/core/ports"
+	"go.opentelemetry.io/otel/trace"
 )
 
+// stdoutAdapter is an internal implementation of the ports.Logger interface
+// that writes logs to standard output in JSON format.
 type stdoutAdapter struct {
 	logger *slog.Logger
 }
 
-func NewStdoutAdapter() ports.Logger {
-	hanlder := slog.NewJSONHandler(os.Stdout, nil)
+// NewStdoutLogger initializes and returns a new Logger that outputs to stdout.
+// It uses slog's JSONHandler for structured logging.
+func NewStdoutLogger() ports.Logger {
+	handler := slog.NewJSONHandler(os.Stdout, nil)
 	return &stdoutAdapter{
-		logger: slog.New(hanlder),
+		logger: slog.New(handler),
 	}
 }
 
-func (s stdoutAdapter) Debug(msg string, args ...interface{}) {
-	s.logger.Debug(msg, args...)
+func (l *stdoutAdapter) appendTraceInfo(ctx context.Context, args []any) []any {
+	spanContext := trace.SpanFromContext(ctx).SpanContext()
+	if spanContext.HasTraceID() {
+		args = append(args,
+			slog.String("trace_id", spanContext.TraceID().String()),
+			slog.String("span_id", spanContext.SpanID().String()),
+		)
+	}
+	return args
 }
 
-func (s stdoutAdapter) Info(msg string, args ...interface{}) {
-	s.logger.Info(msg, args...)
+func (l *stdoutAdapter) Debug(ctx context.Context, msg string, args ...any) {
+	args = l.appendTraceInfo(ctx, args)
+	l.logger.Debug(msg, args...)
 }
 
-func (s stdoutAdapter) Warn(msg string, args ...interface{}) {
-	s.logger.Warn(msg, args...)
+func (l *stdoutAdapter) Info(ctx context.Context, msg string, args ...any) {
+	args = l.appendTraceInfo(ctx, args)
+	l.logger.Info(msg, args...)
 }
 
-func (s stdoutAdapter) Error(msg string, args ...interface{}) {
-	s.logger.Error(msg, args...)
+func (l *stdoutAdapter) Warn(ctx context.Context, msg string, args ...any) {
+	args = l.appendTraceInfo(ctx, args)
+	l.logger.Warn(msg, args...)
+}
+
+func (l *stdoutAdapter) Error(ctx context.Context, msg string, args ...any) {
+	args = l.appendTraceInfo(ctx, args)
+	l.logger.Error(msg, args...)
 }
