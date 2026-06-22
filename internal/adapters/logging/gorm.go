bd2ac3c7618137b96f238572bf2d01e29a299abe
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-06-22 11:20:16 +0500

    KAN-5: add gorm logger

---
+++ internal/adapters/logging/gorm.go
@@ -0,0 +1,65 @@
+package logging
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/yerdauletzhumabay/backend-mypizza-golang/internal/core/ports"
+	gormlogger "gorm.io/gorm/logger"
+)
+
+type gormAdapter struct {
+	logger ports.Logger
+	config gormlogger.Config
+}
+
+// NewGormAdapter bridges the ports.Logger to GORM's requirements
+func NewGormAdapter(l ports.Logger, cfg gormlogger.Config) gormlogger.Interface {
+	return &gormAdapter{
+		logger: l,
+		config: cfg,
+	}
+}
+
+func (a *gormAdapter) LogMode(level gormlogger.LogLevel) gormlogger.Interface {
+	newConfig := a.config
+	newConfig.LogLevel = level
+	return &gormAdapter{logger: a.logger, config: newConfig}
+}
+
+func (a *gormAdapter) Info(ctx context.Context, msg string, args ...interface{}) {
+	if a.config.LogLevel >= gormlogger.Info {
+		a.logger.Debug(fmt.Sprintf(msg, args...))
+	}
+}
+
+func (a *gormAdapter) Warn(ctx context.Context, msg string, args ...interface{}) {
+	if a.config.LogLevel >= gormlogger.Warn {
+		a.logger.Warn(fmt.Sprintf(msg, args...))
+	}
+}
+
+func (a *gormAdapter) Error(ctx context.Context, msg string, args ...interface{}) {
+	if a.config.LogLevel >= gormlogger.Error {
+		a.logger.Error(fmt.Sprintf(msg, args...))
+	}
+}
+
+func (a *gormAdapter) Trace(ctx context.Context, begin time.Time, fc func() (string, int64), err error) {
+	if a.config.LogLevel <= gormlogger.Silent {
+		return
+	}
+
+	elapsed := time.Since(begin)
+	sql, rows := fc()
+
+	switch {
+	case err != nil && a.config.LogLevel >= gormlogger.Error:
+		a.logger.Error("GORM Query Error", "err", err, "elapsed", elapsed, "rows", rows, "sql", sql)
+	case elapsed > a.config.SlowThreshold && a.config.SlowThreshold != 0 && a.config.LogLevel >= gormlogger.Warn:
+		a.logger.Warn("GORM Slow Query", "elapsed", elapsed, "rows", rows, "sql", sql)
+	case a.config.LogLevel >= gormlogger.Info:
+		a.logger.Info("GORM Query", "elapsed", elapsed, "rows", rows, "sql", sql)
+	}
+}
