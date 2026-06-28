16ea63da9eb2c782797d658a1a8e09de543d512c
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-06-28 12:00:34 +0500

    KAN-12: table CityProductItem now hold Price and Currency columns

--- internal/adapters/repository/postgresql/persistency/product/city-product-item-mtm.go
+++ internal/adapters/repository/postgresql/persistency/product/city-product-item-mtm.go
@@ -8,9 +8,11 @@ import (
 
 // nolint:govet
 type CityProductItem struct {
-	CityID        uuid.UUID `gorm:"type:uuid;primaryKey"`
-	ProductItemID uuid.UUID `gorm:"type:uuid;primaryKey"`
-	IsAvailable   bool      `gorm:"type:boolean;index;default:true;not null"`
+	CityID        uuid.UUID    `gorm:"type:uuid;primaryKey"`
+	ProductItemID uuid.UUID    `gorm:"type:uuid;primaryKey"`
+	Price         int64        `gorm:"type:integer;not null"`
+	Currency      CurrencyName `gorm:"type:currency_name;not null;default:'KZT'"`
+	IsAvailable   bool         `gorm:"type:boolean;index;default:true;not null"`
 
 	UpdatedAt time.Time `gorm:"type:timestamptz;default:now();not null"`
 
