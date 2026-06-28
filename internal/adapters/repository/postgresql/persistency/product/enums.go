6cbcd51a22e58d76dbf4fa4f4d68226c593de9bb
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-06-28 11:58:02 +0500

    KAN-12: remove enums: item_size and item_type

--- internal/adapters/repository/postgresql/persistency/product/enums.go
+++ internal/adapters/repository/postgresql/persistency/product/enums.go
@@ -1,29 +1,7 @@
 package product
 
 // Enums
-type ItemSize string
-type ItemType string
 type CurrencyName string
-
-const (
-	// Pizza Sizes
-	Size20 ItemSize = "20см"
-	Size25 ItemSize = "25см"
-	Size30 ItemSize = "30см"
-	Size35 ItemSize = "35см"
-
-	// Item Types
-	Traditional ItemType = "Традиционный"
-	Thin        ItemType = "Тонкое"
-	Small       ItemType = "Маленькая"
-	Big         ItemType = "Большая"
-
-	// Drink Sizes
-	Size03L  ItemSize = "0.3L"
-	Size045L ItemSize = "0.45L"
-	Size05L  ItemSize = "0.5L"
-	Size1L   ItemSize = "1.0L"
-)
 
 const (
 	CurrencyKZT CurrencyName = "KZT"
