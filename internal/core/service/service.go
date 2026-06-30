828d4b18b358f2ae07c09a1e08b86b32b70177fb
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-06-30 19:36:04 +0500

    KAN-15: add product GetCityAllCategoriesProducts domain method implementation

---
+++ internal/core/service/service.go
@@ -0,0 +1,24 @@
+package service
+
+import (
+	"context"
+
+	"github.com/yerdauletzhumabay/backend-mypizza-golang/internal/core/domain"
+	"github.com/yerdauletzhumabay/backend-mypizza-golang/internal/core/ports"
+)
+
+type service struct {
+	repo   ports.ProductRepository
+	logger ports.Logger
+}
+
+func NewProductService(repo ports.ProductRepository, logger ports.Logger) ports.ProductService {
+	return &service{
+		repo:   repo,
+		logger: logger,
+	}
+}
+
+func (s *service) GetCityAllCategoriesProducts(ctx context.Context, cityName string) (*domain.City, error) {
+	return s.repo.GetCityAllCategoriesProducts(ctx, cityName)
+}
