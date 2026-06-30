290f35ffb1466076ef2f95cfe1ae8b1a1565ac19
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-06-30 19:35:27 +0500

    KAN-15: add Product Pizza related constants for now it is not used

--- internal/core/domain/constants.go
+++ internal/core/domain/constants.go
@@ -10,3 +10,19 @@ const (
 	LogLevelMiddleware = "Middleware"
 	LogLevelCache      = "Cache"
 )
+
+// Product Pizza related constants
+// PizzaSizes and PizzaType are served as enums which used to validate when inserting product or item to the db or
+// To Decide from which Pizza type and size to start search to get product image and price to show clients in the main page
+type PizzaSizes string
+type PizzaTypes string
+
+const (
+	TraditionalPizza PizzaTypes = "Традиционное"
+	ThinPizza        PizzaTypes = "Тонкое"
+
+	PizzaSize20 PizzaSizes = "20см"
+	PizzaSize25 PizzaSizes = "25см"
+	PizzaSize30 PizzaSizes = "30см"
+	PizzaSize35 PizzaSizes = "35см"
+)
