7eb24301aff060cc529b57043bfd252c1adfc278
Yerdaulet Zhumabay <yerdaulet.zhumabay@gmail.com>
2026-07-01 10:58:23 +0000

    KAN-16: adjust the 01_seed_deletion to TURNCATE effectivelly every data from...

--- internal/adapters/repository/postgresql/persistency/product/city-product-item-mtm.go
+++ internal/adapters/repository/postgresql/persistency/product/city-product-item-mtm.go
@@ -8,11 +8,14 @@ import (
 
 // nolint:govet
 type CityProductItem struct {
-	CityID        uuid.UUID    `gorm:"type:uuid;primaryKey"`
-	ProductItemID uuid.UUID    `gorm:"type:uuid;primaryKey"`
-	Price         int64        `gorm:"type:integer;not null"`
-	Currency      CurrencyName `gorm:"type:currency_name;not null;default:'KZT'"`
-	IsAvailable   bool         `gorm:"type:boolean;index;default:true;not null"`
+	CityID        uuid.UUID `gorm:"type:uuid;primaryKey;uniqueIndex:idx_one_displayed_item_per_product_per_city,where:is_displayed = true"`
+	ProductItemID uuid.UUID `gorm:"type:uuid;primaryKey"`
+	ProductID     uuid.UUID `gorm:"type:uuid;uniqueIndex:idx_one_displayed_item_per_product_per_city,where:is_displayed = true"`
+
+	Price       int64        `gorm:"type:integer;not null"`
+	Currency    CurrencyName `gorm:"type:currency_name;not null;default:'KZT'"`
+	IsAvailable bool         `gorm:"type:boolean;index;default:true;not null"`
+	IsDisplayed bool         `gorm:"type:boolean;index;default:false;not null"`
 
 	UpdatedAt time.Time `gorm:"type:timestamptz;default:now();not null"`
 
