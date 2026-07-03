5d4c1a0fa4a00167c9e1535c0b73b967b871685d
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-07-03 21:29:20 +0500

    KAN-17: add handler structs which related to this method GetCityAllCategoryProductsRequest to map from domain product structs

--- internal/adapters/handlers/dto/product.go
+++ internal/adapters/handlers/dto/product.go
@@ -3,12 +3,34 @@ package dto
 
 import "github.com/google/uuid"
 
+// START of GetCityAllCategoryProducts
+
 type GetCityAllCategoryProductsRequest struct {
 	CityName string `json:"city_name" validate:"required,min=3,max=50"`
 }
 
+// nolint:govet
 type GetCityAllCategoryProductsResponse struct {
-	Name     string    `json:"name"`
-	ImageUrl string    `json:"image_url"`
-	ID       uuid.UUID `json:"id"`
+	Categories []CatalogCategory `json:"categories"`
+	ID         uuid.UUID         `json:"id"`
 }
+
+// nolint:govet
+type CatalogCategory struct {
+	ID           uuid.UUID        `json:"id"`
+	Name         string           `json:"name"`
+	DisplayOrder int              `json:"display_order"`
+	Products     []CatalogProduct `json:"products"`
+}
+
+// nolint:govet
+type CatalogProduct struct {
+	ID           uuid.UUID `json:"id"`
+	Name         string    `json:"name"`
+	ImageUrl     string    `json:"image_url"`
+	Price        int64     `json:"price"`
+	Currency     string    `json:"currency"`
+	DisplayOrder int       `json:"display_order"`
+}
+
+// END of GetCityAllCategoryProducts
