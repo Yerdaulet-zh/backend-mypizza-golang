aefc94bc56f33b2d56b84a2bd6fa72a0b382f4d2
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-07-08 16:39:22 +0500

    KAN-19: add CatalogProductQuery interface method for repo and service ports

--- internal/core/ports/product.go
+++ internal/core/ports/product.go
@@ -4,12 +4,15 @@ import (
 	"context"
 
 	"github.com/yerdauletzhumabay/backend-mypizza-golang/internal/core/domain"
+	"github.com/yerdauletzhumabay/backend-mypizza-golang/internal/core/domain/dproduct"
 )
 
 type ProductService interface {
 	GetCityAllCategoriesProducts(ctx context.Context, cityName string) (*domain.City, error)
+	CatalogProductQuery(ctx context.Context, cityName string, query string) ([]*dproduct.ProductCatalogQuerySearchResult, error)
 }
 
 type ProductRepository interface {
 	GetCityAllCategoriesProducts(ctx context.Context, cityName string) (*domain.City, error)
+	CatalogProductQuery(ctx context.Context, cityName string, query string) ([]*dproduct.ProductCatalogQuerySearchResult, error)
 }
