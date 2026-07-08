356963c59837fc6d5d520d601450c0ba8c2a6f97
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-07-08 16:42:27 +0500

    KAN-19: add service TestCatalogProductQuery which just queries the repo.TestCatalogProductQuery

--- internal/core/service/product.go
+++ internal/core/service/product.go
@@ -6,6 +6,7 @@ import (
 	"context"
 
 	"github.com/yerdauletzhumabay/backend-mypizza-golang/internal/core/domain"
+	"github.com/yerdauletzhumabay/backend-mypizza-golang/internal/core/domain/dproduct"
 	"github.com/yerdauletzhumabay/backend-mypizza-golang/internal/core/ports"
 	"go.opentelemetry.io/otel"
 )
@@ -25,5 +26,12 @@ func NewProductService(repo ports.ProductRepository, logger ports.Logger) ports.
 func (s *service) GetCityAllCategoriesProducts(ctx context.Context, cityName string) (*domain.City, error) {
 	ctx, span := otel.Tracer("service").Start(ctx, "service.GetCityAllCategoriesProducts")
 	defer span.End()
+
 	return s.repo.GetCityAllCategoriesProducts(ctx, cityName)
+}
+
+func (s *service) CatalogProductQuery(ctx context.Context, cityName string, query string) ([]*dproduct.ProductCatalogQuerySearchResult, error) {
+	ctx, span := otel.Tracer("service").Start(ctx, "service.CatalogProductQuery")
+	defer span.End()
+	return s.repo.CatalogProductQuery(ctx, cityName, query)
 }
