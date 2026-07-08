16b4e7afbf8bc80280aac1e462a404a9d495fa6c
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-07-08 17:03:20 +0500

    KAN-19: Add the trigram extention to the integration test before database migrations

--- internal/adapters/repository/postgresql/repo/product-query_test.go
+++ internal/adapters/repository/postgresql/repo/product-query_test.go
@@ -47,6 +47,11 @@ func (c *CatalogProductQueryIntegrationTestSuite) SetupSuite() {
 		c.T().Fatalf("Failed to dynamically initialize custom database enum types: %v", err)
 	}
 
+	// Install pg_trgm on the clean test DB
+	if err := c.db.Exec("CREATE EXTENSION IF NOT EXISTS pg_trgm;").Error; err != nil {
+		c.T().Fatalf("failed to create pg_trgm extension: %v", err)
+	}
+
 	err = c.db.AutoMigrate(
 		&product.City{},
 		&product.Product{},
